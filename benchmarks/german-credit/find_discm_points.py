22fb0fd5ebd8a27b902052cef8b256ddf98159ab
Sahil Verma <vsahil@iitk.ac.in>
2020-02-23 20:14:28 -0800

    minor restructing in german credit directory, also added comparison with other techniques

---
+++ benchmarks/german-credit/find_discm_points.py
@@ -0,0 +1,128 @@
+import numpy as np
+import copy
+
+
+def rescale_input_numpy(inp):
+    assert(inp.shape[1] == 20)  # 20 features for german credit dataset
+    means_and_ranges = [(1.001, 3), (20.903, 68), (32.545, 4), (47.148, 370), (3271.258, 18174), (1.19, 4), (3.384, 4), (2.973, 3), (0.69, 1), (101.145, 2), (2.845, 3), (122.358, 3), (35.546, 56), (142.675, 2), (151.929, 2), (1.407, 3), (172.904, 3), (1.155, 1), (1.404, 1), (1.037, 1)]
+    r = np.arange(20)
+    out = copy.deepcopy(inp)
+    for col, (mean, range_) in zip(r, means_and_ranges):
+        out[:, col] = np.divide(np.subtract(out[:, col], mean), range_)
+    return out
+
+
+def rescale_input_numpy_reweighted_german(inp):
+    assert(inp.shape[1] == 20)  # 20 features for german credit dataset
+    # means_and_ranges = [(1.001, 3), (20.903, 68), (32.545, 4), (47.148, 370), (3271.258, 18174), (1.19, 4), (3.384, 4), (2.973, 3), (0.69, 1), (101.145, 2), (2.845, 3), (122.358, 3), (35.546, 56), (142.675, 2), (151.929, 2), (1.407, 3), (172.904, 3), (1.155, 1), (1.404, 1), (1.037, 1)]
+    means_and_ranges = [(1.001, 3), (20.903, 68), (32.545, 4), (47.148, 370), (3271.258, 18174), (1.19, 4), (3.384, 4), (2.973, 3), (0.69, 1), (101.145, 2), (2.845, 3), (122.358, 3), (35.546, 56), (142.675, 2), (151.929, 2), (1.407, 3), (172.904, 3), (1.155, 1), (1.404, 1), (1.037, 1)]
+    r = np.arange(20)
+    out = copy.deepcopy(inp)
+    for col, (mean, range_) in zip(r, means_and_ranges):
+        out[:, col] = np.divide(np.subtract(out[:, col], mean), range_)
+    return out
+
+
+def rescale_input_numpy_disparateremoved_german(inp):
+    assert(inp.shape[1] == 20)  # 20 features for german credit dataset
+    # means_and_ranges = [(1.001, 3), (20.903, 68), (32.545, 4), (47.148, 370), (3271.258, 18174), (1.19, 4), (3.384, 4), (2.973, 3), (0.69, 1), (101.145, 2), (2.845, 3), (122.358, 3), (35.546, 56), (142.675, 2), (151.929, 2), (1.407, 3), (172.904, 3), (1.155, 1), (1.404, 1), (1.037, 1)]
+    means_and_ranges  = [(1.001, 3), (19.172, 56), (32.545, 4), (47.148, 370), (2866.523, 15607), (1.19, 4), (3.384, 4), (2.973, 3), (0.69, 1), (101.145, 2), (2.845, 3), (122.358, 3), (34.434, 56), (142.675, 2), (151.929, 2), (1.407, 3), (172.904, 3), (1.155, 1), (1.404, 1), (1.037, 1)]
+    r = np.arange(20)
+    out = copy.deepcopy(inp)
+    for col, (mean, range_) in zip(r, means_and_ranges):
+        out[:, col] = np.divide(np.subtract(out[:, col], mean), range_)
+    return out
+
+
+def entire_test_suite(mini=True, reweighted_german=False, disparateremoved=False):
+    gender0 = "gender0"
+    gender1 = "gender1"
+    if mini:
+        gender0 += "_mini"
+        gender1 += "_mini"
+    class0_ = np.genfromtxt(f"/Users/sahilverma/research/influence-duplicate/german-credit-dataset/{gender0}.csv", delimiter=",")
+    class1_ = np.genfromtxt(f"/Users/sahilverma/research/influence-duplicate/german-credit-dataset/{gender1}.csv", delimiter=",")
+
+    assert(not (reweighted_german and disparateremoved)),  "both can't be true at the same time"
+    
+    if reweighted_german:
+        class0 = rescale_input_numpy_reweighted_german(class0_)
+        class1 = rescale_input_numpy_reweighted_german(class1_)
+    elif disparateremoved:
+        class0 = rescale_input_numpy_disparateremoved_german(class0_)
+        class1 = rescale_input_numpy_disparateremoved_german(class1_)
+    else:
+        class0 = rescale_input_numpy(class0_)
+        class1 = rescale_input_numpy(class1_)
+
+    return class0, class1
+    
+    # length = class0.shape[0]
+    # l_zero = np.eye(2)[np.zeros(length, dtype=int)]     # list of zero labels
+    # l_one = np.eye(2)[np.ones(length, dtype=int)]      # list of one labels
+
+    # This all never happens
+    with tf.Session() as sess:
+        out0, loss_class0_label_0 = sess.run([logits, loss_no_reg], feed_dict={X: class0, Y:l_zero})
+        out0, loss_class0_label_1 = sess.run([logits, loss_no_reg], feed_dict={X: class0, Y:l_one})
+        
+        out1, loss_class1_label_0 = sess.run([logits, loss_no_reg], feed_dict={X: class1, Y:l_zero})
+        out1, loss_class1_label_1 = sess.run([logits, loss_no_reg], feed_dict={X: class1, Y:l_one})
+    
+        out0_ = np.argmax(out0, axis = 1)
+        out1_ = np.argmax(out1, axis = 1)
+        # print("hello")
+        print(sum(out0_ != out1_))
+        
+        idx = np.where(out0_ != out1_)[0]
+        g0 = class0_[idx]
+        g1 = class1_[idx]
+        write = False
+        if write:
+            with open("tests_german_tf.csv", "w") as f:
+                for x, y in zip(g0, g1):
+                    x = str(tuple(x))[1:-1]
+                    y = str(tuple(y))[1:-1]
+                    z = (x, y)
+                    f.write(str(z) + "\n")
+        
+        # selection of the labels is due to the sum of losses of the pair, 
+        # but the final ranking should be only based on the loss of the data-point which is changing prediction
+        # and the model's prediction for it
+        # import ipdb; ipdb.set_trace()
+        zero_labels_loss = loss_class1_label_0[idx] + loss_class0_label_0[idx]
+        ones_labels_loss = loss_class1_label_1[idx] + loss_class0_label_1[idx]
+        # keep the lower loss for each pair of discriminating test. 
+        loss_labelling = list(map(lambda x: x[1] if x[0] > x[1] else x[0], zip(zero_labels_loss, ones_labels_loss)))
+        desired_labels = list(map(lambda x: 1 if x[0] > x[1] else 0, zip(zero_labels_loss, ones_labels_loss)))
+        undesired_actual_labels = list(map(lambda x: 0 if x == 1 else 1, desired_labels))      # just the inverse of it
+
+        l00 = loss_class0_label_0[idx]
+        l10 = loss_class1_label_0[idx]
+        l11 = loss_class1_label_1[idx]
+        l01 = loss_class0_label_1[idx]
+
+        # for the data point whose prediction == label, choose its complement data-point (gender flipped)
+        which_data_point = list(map(lambda x: (x[3], 1) if int(x[0]) == int(x[1]) else (x[2], 0), zip(out0_[idx], desired_labels, g0, g1)))
+        # import ipdb; ipdb.set_trace()
+        gender = [i[1] for i in which_data_point]
+        which_data_point = [i[0] for i in which_data_point]
+
+        losses_at_this_point = list(map(lambda x: x[2] if x[0] == 0 and x[1] == 0 else (x[3] if x[0] == 0 and x[1] == 1 else (x[4] if x[0] == 1 and x[1] == 0 else x[5])) , zip(gender, undesired_actual_labels, l00, l01, l10, l11)))
+
+        # arg_ = np.argsort(loss_labelling).tolist()
+        arg_ = np.argsort(losses_at_this_point).tolist()
+        undesired_actual_labels_sorted = [undesired_actual_labels[i] for i in arg_]
+        which_data_point_sorted = [which_data_point[i] for i in arg_]
+        
+        # this should not be desired labels as the prediction of the model is not the desired label and we want the point responsible for that
+        # with open("scheme8_labelled_generated_tests.csv", "w") as f:
+        #     for dt, la in zip(which_data_point_sorted, desired_labels_sorted):
+        #         f.write(str(dt.astype(int).tolist())[1:-1] + ", " + str(la) + "\n")       # don't take the scaled and modified input
+
+        with open("scheme8_labelled_generated_tests.csv", "w") as f:
+            f.write("Checking-ccount,Months,Credit-history,Purpose,Credit-mount,Savings-ccount,Present-employment-since,Instllment-rte,Gender,Other-debtors,Present-residence-since,Property,age,Other-instllment-plns,Housing,Number-of-existing-credits,Job,Number-of-people-being-lible,Telephone,Foreign-worker, Final-label\n")
+            for dt, la in zip(which_data_point_sorted, undesired_actual_labels_sorted):
+                f.write(str(dt.astype(int).tolist())[1:-1] + ", " + str(la) + "\n")
+
+
---
+++ benchmarks/german-credit/train_german_credit.py
@@ -0,0 +1,117 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals  
+
+import numpy as np
+import IPython, sys, os
+sys.path.append(".")
+sys.path.append("../")
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+import influence.experiments as experiments
+from influence.fully_connected import Fully_Connected
+
+from load_german_credit import load_german_credit, load_german_credit_partial
+from find_discm_points import entire_test_suite
+
+data_sets = load_german_credit()
+# data_sets = reweighted_load_german_credit()
+
+input_dim = 20
+weight_decay = 0.001
+batch_size = 50
+
+initial_learning_rate = 0.005 
+decay_epochs = [40000]
+hidden1_units = 16
+hidden2_units = 8
+num_classes = 2
+keep_probs = [1.0, 1.0]
+
+scheme = 8
+assert(scheme == 8)     # now always
+model = Fully_Connected(
+    input_dim=input_dim, 
+    hidden1_units=hidden1_units, 
+    hidden2_units=hidden2_units,
+    weight_decay=weight_decay,
+    num_classes=num_classes, 
+    batch_size=batch_size,
+    data_sets=data_sets,
+    initial_learning_rate=initial_learning_rate,
+    damping=1e-2,
+    decay_epochs=decay_epochs,
+    mini_batch=True,
+    train_dir='output', 
+    log_dir='log',
+    hvp_files = "inverse_HVP_scheme{}".format(scheme),
+    model_name='german_credit_try1',
+    scheme = "scheme{}".format(scheme))
+
+num_steps = batch_size * 1000
+# model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000)
+
+iter_to_load = num_steps - 1
+# iter_to_load = 46999
+model.load_checkpoint(iter_to_load=iter_to_load, do_checks=False)
+class0_data, class1_data = entire_test_suite(False)     # False means loads entire data
+# print(model.data_sets.train.num_examples)
+# exit(0)
+model.find_discm_examples(class0_data, class1_data, print_file=True, scheme=scheme)
+
+
+# test_idx = 0
+
+predicted_loss_diffs = model.get_influence_on_test_loss(
+            [i for i in range(model.discm_data_set.num_examples)], 
+            np.arange(len(model.data_sets.train.labels)),
+            force_refresh=False)
+# import ipdb; ipdb.set_trace()
+sorted_training_points = list(np.argsort(predicted_loss_diffs)[::-1])     # decreasing order of influence among training points
+# print(sorted_training_points)
+
+exit(0)
+training_size = model.data_sets.train.num_examples
+assert(len(sorted_training_points) == training_size)
+
+del model   # so that weights of the original model are not used. This will not help
+
+# for percentage in range(5, 4, 0.5):
+for percentage in np.arange(0, 5.0, 0.2):
+    tf.reset_default_graph()
+    p = int(training_size * percentage / 100)
+    # p = 19
+    remaining_indexes = np.array(sorted_training_points[p:])
+    data_sets_partial = load_german_credit_partial(remaining_indexes)
+    assert(data_sets_partial.train.num_examples == 750 - p)
+    model_partial_data = Fully_Connected(
+        input_dim=input_dim, 
+        hidden1_units=hidden1_units, 
+        hidden2_units=hidden2_units,
+        weight_decay=weight_decay,
+        num_classes=num_classes, 
+        batch_size=batch_size,
+        data_sets=data_sets_partial,
+        initial_learning_rate=initial_learning_rate,
+        damping=1e-2,
+        decay_epochs=decay_epochs,
+        mini_batch=False,
+        train_dir='output_partial', 
+        log_dir='log_partial',
+        hvp_files = "inverse_HVP_scheme1_",
+        model_name='german_credit_partial',
+        scheme = "scheme1_")
+    print("Training")
+    model_partial_data.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, verbose=False)
+    print("Percentage: ", percentage, " Points removed: ", p)
+    num = model_partial_data.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
+    with open("scheme{}_results.txt".format(scheme), "a") as f:
+        f.write("Percentage: " + str(percentage) + ", Discriminating Tests: " + str(num) + "\n")
+    
+    del model_partial_data          # to remove any chance of reusing variables and reduce memory
+
+
