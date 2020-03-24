1fc192c6140209a44b5a2c300f723c4c7b641d0e
Sahil Verma <vsahil@iitk.ac.in>
2020-03-23 22:45:05 -0700

    added learning fair representations code for default dataset

--- benchmarks/default/load_default.py
+++ benchmarks/default/load_default.py
@@ -86,6 +86,28 @@ def load_default(perm=-1, validation_size=0):
 	return base.Datasets(train=train, validation=validation, test=test)
 
 
+def load_fair_representations(perm, total_dataset, total_labels, validation_size=0):
+	assert(perm < 20)		# we only have 20 permutations
+	if perm >= 0:	# for negative number don't do
+		ordering = permutations(perm)
+		total_dataset, total_labels = total_dataset[ordering], total_labels[ordering]
+
+	# no. of 1's in adult dataset is 11208, and 8947 in training set.
+	train_examples = 24000		# testing set is 6000		# exactly 20%
+	X_train = total_dataset[:train_examples]
+	X_validation = total_dataset[train_examples:train_examples + validation_size]
+	X_test  = total_dataset[train_examples + validation_size:]
+	Y_train = total_labels[:train_examples]
+	Y_validation = total_labels[train_examples:train_examples + validation_size]
+	Y_test  = total_labels[train_examples + validation_size:]
+	assert(len(Y_test) == 6000)
+	train = DataSet(X_train, Y_train)
+	validation = DataSet(X_validation, Y_validation)
+	test = DataSet(X_test, Y_test)
+
+	return base.Datasets(train=train, validation=validation, test=test)
+
+
 def disparate_removed_load_default(perm, validation_size=0):
 	total_dataset = genfromtxt(f"{os.path.dirname(os.path.realpath(__file__))}/disparate_impact_removed/normalized_disparateremoved_features-default.csv", delimiter=",")      # this is the standarised/normalised data, so no need to renormalize
 	total_labels = genfromtxt(f"{os.path.dirname(os.path.realpath(__file__))}/disparate_impact_removed/normalized_disparateremoved_labels-default.csv", delimiter=",")
