ac43f9699e6c4868ab58a0fe0273e7f1846d098f
Sahil Verma <vsahil@iitk.ac.in>
2020-04-16 21:07:52 -0700

    Made changes to print to latex table and to adjust the order of algorithms in box plot- made our technique at last

--- benchmarks/faceted_box_plots_all_datasets.py
+++ benchmarks/faceted_box_plots_all_datasets.py
@@ -31,25 +31,25 @@ def process_dfs(name, batches, df):
     assert len(df['Model-count'].unique()) == 240 and df['Model-count'].max() == 239 and df['Model-count'].min() == 0
     df = df.sort_values("Discm_percent").groupby("Model-count", as_index=False).first()     # must be sorted in order of model count for comparison across baselines
     df = df[['Model-count','Discm_percent']]
-    df['Baseline'] = name
+    df['Techniques'] = name
     if len(name.split()) > 1:
         words = name.split()
         letters = [word[0] for word in words]
         x = "".join(letters)
-        df['Technique'] = x
+        df['Baseline'] = x
     else:    
-        df['Technique'] = name[:2]
+        df['Baseline'] = name[:2]
     return df
 
 
-def boxplots_datasets(dataset):
+def boxplots_datasets(dataset, plot):
     df1 = pd.read_csv(f"{dataset}/results_{dataset}_final.csv")
     batches = list(df1.Batch.unique())
     assert(len(batches) == 2)
     df_our = find_min_discm_each_hyperparam(df1)
     df_our = df_our[['Model-count','Discm_percent']]
+    df_our['Techniques'] = "Our Technique"
     df_our['Baseline'] = "Our"
-    df_our['Technique'] = "Our"
 
     # Massaging
     df_massaging = process_dfs("Massaging", batches, pd.read_csv(f"{dataset}/massaging/results_massaged_{dataset}.csv"))
@@ -64,37 +64,82 @@ def boxplots_datasets(dataset):
     df_adver['Model-count'] = df_adver['Dataperm']*12
     df_adver = df_adver.sort_values("Discm_percent").groupby("Model-count", as_index=False).first()     # must be sorted in order of model count for comparison across baselines
     df_adver = df_adver[['Model-count','Discm_percent']]
-    df_adver['Baseline'] = "Adver. Debias"
-    df_adver['Technique'] = "AD"
+    df_adver['Techniques'] = "Adversa. debias"
+    df_adver['Baseline'] = "AD"
 
     df_main = pd.concat([df_our, df_massaging, df_ps, df_lfr, df_DIR, df_adver])
     assert(len(df_main) == 5*240 + 20)
     df_main['Dataset'] = dataset.capitalize()
+
+    if dataset == "german":
+        sizeofPSI = 100000
+    elif dataset == "adult":
+        sizeofPSI = 4522200
+    elif dataset == "student":
+        sizeofPSI = 100000
+    elif dataset == "default":
+        sizeofPSI = 3000000
+    else:
+        raise NotImplementedError
+    
+    rounding = 3
+
+    if plot == 0:
+        x = ' & '.join([f"{dataset.capitalize()}", f"{sizeofPSI}", f"{df_DIR['Discm_percent'].min().round(rounding)}", f"{df_ps['Discm_percent'].min().round(rounding)}", f"{df_massaging['Discm_percent'].min().round(rounding)}", f"{df_lfr['Discm_percent'].min().round(rounding)}", f"{df_adver['Discm_percent'].min().round(rounding)}", "\\textbf{%s}"%(f"{df_our['Discm_percent'].min().round(rounding)}")])
+        if dataset == "adult":
+            mode = "w"
+        else:
+            mode = "a"
+        with open("../../neurips_fairness_paper/min-discm.tex", mode) as f:
+            if not dataset == "default":
+                x += "  \\\  \hline"
+            else:
+                x += "  \\\\  "
+            print(x, file=f)
+        
+        # print(dataset)
+        # print("Min discm: ", df_DIR['Discm_percent'].min())
+        # print("Min discm: ", df_ps['Discm_percent'].min())
+        # print("Min discm: ", df_massaging['Discm_percent'].min())
+        # print("Min discm: ", df_lfr['Discm_percent'].min())
+        # print("Min discm: ", df_adver['Discm_percent'].min())
+        # print("Min discm: ", df_our['Discm_percent'].min())
+    
     return df_main
 
 
-df_main = None
-benchmarks = ["german", "adult", "default", "student"]
-for dataset in benchmarks:
-# for dataset in ["german", "student"]:
-    df_onedataset = boxplots_datasets(dataset)
-    if not df_main is None:
-        df_main = pd.concat([df_main, df_onedataset])
-    else:
-        df_main = copy.deepcopy(df_onedataset)
-    print(f"Done {dataset}")
+def main(plot):
+    df_main = None
+    benchmarks = ["adult", "german", "student", "default"]
+    for dataset in benchmarks:
+    # for dataset in ["adult", "default"]:
+        df_onedataset = boxplots_datasets(dataset, plot)
+        if not df_main is None:
+            df_main = pd.concat([df_main, df_onedataset])
+        else:
+            df_main = copy.deepcopy(df_onedataset)
+        print(f"Done {dataset}")
 
-# import ipdb; ipdb.set_trace()
+    if plot == 0:
+        return 
+    # import ipdb; ipdb.set_trace()
+    labels = ['DIR', 'PS', 'Ma', 'LFR', 'AD', 'Our']
+    tech_cat = pd.Categorical(df_main['Baseline'], categories=labels)
+    df_main = df_main.assign(Technique_x = tech_cat)
+    
+    x = (ggplot(aes(x='Technique_x', y='Discm_percent', color='Techniques'), data=df_main) +\
+            geom_boxplot() +\
+            facet_wrap(['Dataset'], scales = 'free', nrow=1, ncol=len(benchmarks), labeller='label_both', shrink=False) + \
+            ylab("Remaining Individual Discrimination") + \
+            xlab("Discrimination reducing techniques") + \
+            # ggtitle("Box plot showing remaining discrimination for each technique in each dataset") +\
+            theme(axis_text_x = element_text(size=6), dpi=151) +\
+            theme_seaborn()
+            )
 
-x = (ggplot(aes(x='Technique', y='Discm_percent',color='Baseline'), data=df_main) +\
-        geom_boxplot() +\
-        facet_wrap(['Dataset'], scales = 'free', nrow=1, ncol=len(benchmarks), labeller='label_both', shrink=False) + \
-        ylab("Remaining Individual Discrimination") + \
-        xlab("Discrimination reducing techniques") + \
-        # ggtitle("Box plot showing remaining discrimination for each technique in each dataset") +\
-        theme(axis_text_x = element_text(size=6), dpi=151) +\
-        theme_seaborn()
-        )
+    x.save(f"boxplot_discm_freeaxis.png", height=5, width=12)
+    # x.save(f"boxplot_discm_fixedaxis.png", height=5, width=12)
 
-x.save(f"boxplot_discm_freeaxis.png", height=5, width=12)
-# x.save(f"boxplot_discm_fixedaxis.png", height=5, width=12)
+if __name__ == "__main__":
+    plot = int(sys.argv[1])
+    main(plot)
