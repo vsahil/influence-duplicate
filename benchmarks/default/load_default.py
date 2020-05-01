1cd9f892abc8716a447efdffe1692947d5f5444b
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-01 16:37:19 -0700

    forgot to push

--- benchmarks/default/load_default.py
+++ benchmarks/default/load_default.py
@@ -62,6 +62,30 @@ def exclude_some_examples(exclude, validation_size=0, remove_biased_test=True):
 	return base.Datasets(train=train, validation=validation, test=test)
 
 
+def load_default_nosensitive(perm=-1, validation_size=0):
+	total_dataset = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/../../default-dataset/normalized_default_nosensitive_features.csv").to_numpy()
+	total_labels = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/../../default-dataset/default_labels.csv").to_numpy()
+	total_labels = total_labels.flatten()
+	assert(perm < 20)		# we only have 20 permutations
+	if perm >= 0:	# for negative number don't do
+		ordering = permutations(perm)
+		total_dataset, total_labels = total_dataset[ordering], total_labels[ordering]
+
+	train_examples = 24000		# testing set is 6000		# exactly 20%
+	X_train = total_dataset[:train_examples]
+	X_validation = total_dataset[train_examples:train_examples + validation_size]
+	X_test  = total_dataset[train_examples + validation_size:]
+	Y_train = total_labels[:train_examples]
+	Y_validation = total_labels[train_examples:train_examples + validation_size]
+	Y_test  = total_labels[train_examples + validation_size:]
+	assert(len(Y_test) == 6000)
+	train = DataSet(X_train, Y_train)
+	validation = DataSet(X_validation, Y_validation)
+	test = DataSet(X_test, Y_test)
+
+	return base.Datasets(train=train, validation=validation, test=test)
+
+
 def load_default(perm=-1, validation_size=0):
 	total_dataset = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/../../default-dataset/normalized_default_features.csv").to_numpy()
 	total_labels = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/../../default-dataset/default_labels.csv").to_numpy()
