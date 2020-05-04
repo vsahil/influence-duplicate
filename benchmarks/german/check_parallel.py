59cd2543d70d02f22990aefb9ea497202f6bbc23
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-03 18:20:22 -0700

    conflict

--- benchmarks/german/check_parallel.py
+++ benchmarks/german/check_parallel.py
@@ -7,24 +7,31 @@ def experiment_command(setting, removal_percent):
     os.system(f"python -W ignore train_all_permutations.py {setting} {removal_percent}")
     # process = subprocess.check_output(['python', 'train_all_permutations.py', str(index)], stdout=subprocess.PIPE)
 
-pool = multiprocessing.Pool(120)
-l = [i for i in range(0, 400)]
-# mr = pool.map_async(run_command, l)
-settings = [i for i in range(240)]       # for first 120 settings in total
-mr = pool.starmap_async(experiment_command, product(settings, l))
-while not mr.ready():
-    sys.stdout.flush()
-    mr.wait(0.1)
-print("DONE!")
-
-
-def run_command(setting):
-    os.system(f"python train_all_permutations.py {setting}")
-
 # pool = multiprocessing.Pool(240)
-# l = [i for i in range(240)]
-# mr = pool.map_async(run_command, l)
+# l = [i for i in range(0, 400)]
+# # mr = pool.map_async(run_command, l)
+# settings = [i for i in range(240)]       # for first 120 settings in total
+# mr = pool.starmap_async(experiment_command, product(settings, l))
 # while not mr.ready():
 #     sys.stdout.flush()
 #     mr.wait(0.1)
 # print("DONE!")
+
+
+# def run_command(setting):
+#     os.system(f"python train_all_permutations.py {setting}")
+
+
+def run_discm_find(setting):
+    os.system(f"python train_using_train_test_sets.py {setting}")
+
+
+pool = multiprocessing.Pool(240)
+l = [i for i in range(240)]
+# mr = pool.map_async(run_command, l)
+# mr = pool.map_async(run_nosensitive, l)
+mr = pool.map_async(run_discm_find, l)
+while not mr.ready():
+    sys.stdout.flush()
+    mr.wait(0.1)
+print("DONE!")
