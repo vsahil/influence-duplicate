c140ae430f99357d57bb2250fc14aab35d49872d
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-08 14:42:25 -0700

    this as well

--- benchmarks/adult/train_all_permutations.py
+++ benchmarks/adult/train_all_permutations.py
@@ -20,6 +20,10 @@ from find_discm_points import entire_test_suite
 
 train = False
 
+if not train:
+    x = len(os.listdir('ranking_points_ordered'))
+    assert x == 240
+
 input_dim = 12
 weight_decay = 0.001
 # batch_size = 3000
@@ -42,7 +46,7 @@ def variation(setting_now):
     for perm in range(20):
         for h1units in [16, 24, 32]:
             for h2units in [8, 12]:
-                for batch in [3000, 6000]:      # different batch sizes for this dataset
+                for batch in [2048, 4096]:      # different batch sizes for this dataset
                     if model_count < setting_now:
                         model_count += 1
                         continue
@@ -72,7 +76,7 @@ model = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets,
     initial_learning_rate=initial_learning_rate,
-    damping=1e-2,
+    damping=3e-2,
     decay_epochs=decay_epochs,
     mini_batch=True,
     train_dir=f'trained_models/output_count{model_count}', 
@@ -82,11 +86,13 @@ model = Fully_Connected(
     scheme = f"{scheme}")
 
 
-num_steps = 30000
+num_steps = 10000
 if train:
     model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=True, verbose=False)
 
 ranked_influential_training_points = f"ranking_points_ordered/{name}.npy"
+if not os.path.exists("ranking_points_ordered"):
+    os.mkdir("ranking_points_ordered")
 # if not train and ranking of influential training points is stored in numpy file, then True
 load_from_numpy = False if train else (True if os.path.exists(ranked_influential_training_points) else False)       
 assert(load_from_numpy)
@@ -117,11 +123,11 @@ if not load_from_numpy:
 
 else:
    print("Loading from numpy")
-   initial_num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
-   print(initial_num, "See")
-   size = class0_data.shape[0]/100
-   with open("results_adult_noremoval.csv".format(scheme), "a") as f:
-        f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{initial_num},{initial_num/size}\n")
+#    initial_num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
+#    print(initial_num, "See")
+#    size = class0_data.shape[0]/100
+#    with open("results_adult_noremoval.csv".format(scheme), "a") as f:
+#         f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{initial_num},{initial_num/size}\n")
    sorted_training_points = list(np.load(ranked_influential_training_points))
 
 if train:
@@ -133,15 +139,16 @@ if train:
 removal = int(sys.argv[2])
 # import ipdb; ipdb.set_trace()
 # for p in removal:
+train_size = 45222
 size = class0_data.shape[0]/100
 for percentage in np.linspace(removal-1, removal-0.2, 5):
     tf.reset_default_graph()
-    p = int(36000 * percentage / 100)
+    p = int(train_size * percentage / 100)
     remaining_indexes = np.array(sorted_training_points[p:])
     data_sets_partial = load_adult_income_partial(perm=perm, index=remaining_indexes)
     try:
-        assert(len(remaining_indexes) == 36000 - p)
-        assert(data_sets_partial.train.num_examples == 36000 - p)
+        assert(len(remaining_indexes) == train_size - p)
+        assert(data_sets_partial.train.num_examples == train_size - p)
     except:
         print(p, percentage, removal, data_sets_partial.train.num_examples, "hello")
         assert False
@@ -157,7 +164,7 @@ for percentage in np.linspace(removal-1, removal-0.2, 5):
         initial_learning_rate=initial_learning_rate,
         damping=1e-2,
         decay_epochs=decay_epochs,
-        mini_batch=False,
+        mini_batch=True,
         train_dir='output_partial', 
         log_dir='log_partial',
         hvp_files = "inverse_HVP_scheme1_",
@@ -166,11 +173,11 @@ for percentage in np.linspace(removal-1, removal-0.2, 5):
     print("Training")
     print("Percentage: ", percentage, " Points removed: ", p) 
     model_partial_data.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
-    train_acc, test_acc = model_partial_data.print_model_eval()
-    print("Percentage: ", percentage, " Points removed: ", p)
+    # train_acc, test_acc = model_partial_data.print_model_eval()
+    # print("Percentage: ", percentage, " Points removed: ", p)
     num = model_partial_data.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
-    with open("results_redone_adult.csv".format(scheme), "a") as f:
-        f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{percentage},{p},{num},{num/size}\n")     # the last ones gives percentage of discrimination
+    with open("results_adult_full.csv", "a") as f:
+        f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{percentage},{p},{num},{num/size}\n")     # the last ones gives percentage of discrimination
     
     del model_partial_data          # to remove any chance of reusing variables and reduce memory
 
