e7b35bafa0198c9795bbcaa062168fb0217ed255
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-12 16:02:38 -0700

    Added results for LFR german

--- benchmarks/german/learning_fair_representations/learning_fr.py
+++ benchmarks/german/learning_fair_representations/learning_fr.py
@@ -15,14 +15,15 @@ from aif360.algorithms.preprocessing.lfr import LFR
 
 input_dim = 20
 weight_decay = 0.001
+num_steps = 10000
 
 initial_learning_rate = 1e-4
 decay_epochs = [60000, 70000]
-num_steps = 10000
 num_classes = 2
 keep_probs = [1.0, 1.0]
-scheme = 8
-assert(scheme == 8)     # now always
+damping = 3e-2
+scheme = 8      # ask the model which points are responsible for the current set of predictions
+# assert(scheme == 8)     # now always
 
 setting_now = int(sys.argv[1])
 
@@ -31,12 +32,13 @@ def variation(setting_now):
     for perm in range(20):
         for h1units in [16, 24, 32]:
             for h2units in [8, 12]:
-                for batch in [50, 100]:
+                for batch in [64, 128]:     # different batch sizes for this dataset
                     if model_count < setting_now:
                         model_count += 1
                         continue
                     # print(setting_now, "done", perm, h1units, h2units, batch)
                     return perm, h1units, h2units, batch, model_count
+
 
 perm, h1units, h2units, batch, model_count = variation(setting_now)
 assert(model_count == setting_now)
@@ -48,7 +50,9 @@ dataset_orig = MyGermanDataset(
     permute = perm   
 )
 
-dataset_orig_train, dataset_orig_test = dataset_orig.split([1.0], shuffle=True)
+train_examples = 800
+dataset_orig_train, dataset_orig_test = dataset_orig.split([train_examples], shuffle=False)
+assert(len(dataset_orig_train.convert_to_dataframe()[0]) == train_examples)
 
 privileged_groups = [{'Gender': 1}]
 unprivileged_groups = [{'Gender': 0}]
@@ -57,9 +61,8 @@ TR = TR.fit(dataset_orig_train)
 dataset_transf_train = TR.transform(dataset_orig_train)
 new_df = dataset_transf_train.convert_to_dataframe()[0]
 
-labels = new_df['target'].to_numpy()
-features = new_df.drop(columns=['target']).to_numpy()
-# import ipdb; ipdb.set_trace()
+train_labels = new_df['target'].to_numpy()
+train_features = new_df.drop(columns=['target']).to_numpy()
 
 write = False
 if write:
@@ -69,7 +72,7 @@ if write:
 from load_german_credit import load_fair_representations
 from find_discm_points import entire_test_suite
 
-data_sets = load_fair_representations(perm, features, labels)
+data_sets = load_fair_representations(perm, train_features, train_labels)
 
 hidden1_units = h1units
 hidden2_units = h2units
@@ -87,7 +90,7 @@ model = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets,
     initial_learning_rate=initial_learning_rate,
-    damping=5e-2,
+    damping=damping,
     decay_epochs=decay_epochs,
     mini_batch=True,
     train_dir=f'throw/output_dont_save{model_count}', 
@@ -97,7 +100,8 @@ model = Fully_Connected(
     scheme = f"{scheme}"
     )
 
-model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
+model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, 
+        iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
 train_acc, test_acc = model.print_model_eval()
 class0_data, class1_data = entire_test_suite(mini=False, disparateremoved=False)     # False means loads entire data
 num_dicsm = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
@@ -105,4 +109,4 @@ num_dicsm = model.find_discm_examples(class0_data, class1_data, print_file=False
 print("Discrimination:", num_dicsm)
 size = class0_data.shape[0]/100
 with open("results_lfr_german.csv", "a") as f:
-    f.write(f'{h1units},{h2units},{batch},{perm},{train_acc*100},{test_acc*100},{num_dicsm},{num_dicsm/size}\n')
+    print(f'{h1units},{h2units},{batch},{perm},{train_acc},{test_acc},{num_dicsm},{num_dicsm/size}', file=f)
