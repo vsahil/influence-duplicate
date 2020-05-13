2121e329603e465d3018a5ccb480009e29add387
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-12 23:17:06 -0700

    Added adversarial debiasing results for compas-ground

--- benchmarks/compas-ground/adversarial_debiasing/adversarial_debiasing.py
+++ benchmarks/compas-ground/adversarial_debiasing/adversarial_debiasing.py
@@ -25,7 +25,9 @@ dataset_orig = MyCompasGroundDataset(
     permute=perm 
 )
 
-dataset_orig_train, dataset_orig_test = dataset_orig.split([0.8], shuffle=False)
+train_examples = 5000
+dataset_orig_train, dataset_orig_test = dataset_orig.split([train_examples], shuffle=False)
+assert(len(dataset_orig_train.convert_to_dataframe()[0]) == train_examples)
 
 privileged_groups = [{'race': 1}]
 unprivileged_groups = [{'race': 0}]
