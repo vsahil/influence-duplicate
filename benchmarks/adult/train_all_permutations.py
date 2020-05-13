1359f7233a4f23d5a3e954c06ff2976ad049a327
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-13 14:18:02 -0700

    Added our approach results for adult

--- benchmarks/adult/train_all_permutations.py
+++ benchmarks/adult/train_all_permutations.py
@@ -19,22 +19,20 @@ from load_adult_income import load_adult_income, load_adult_income_partial
 from find_discm_points import entire_test_suite
 
 train = False
+modify_test = True
 
-if not train:
-    x = len(os.listdir('ranking_points_ordered'))
-    assert x == 240
+if not train:       
+    x = len(os.listdir('ranking_points_ordered_method1'))
+    assert x == 240     # This is for checking that none of the model have failed to converge
 
 input_dim = 12
 weight_decay = 0.001
-# batch_size = 3000
 
 initial_learning_rate = 1e-5 
 decay_epochs = [30000, 40000]
-# hidden1_units = 24
-# hidden2_units = 12
-# hidden3_units = 0
 num_classes = 2
 keep_probs = [1.0, 1.0]
+num_steps = 20000
 
 scheme = 8
 assert(scheme == 8)     # now always
@@ -62,6 +60,7 @@ hidden1_units = h1units
 hidden2_units = h2units
 hidden3_units = 0
 batch_size = batch
+damping = 3e-2
 
 print("Start: ", model_count, " Setting: ", perm, hidden1_units, hidden2_units, batch_size)
 
@@ -76,26 +75,25 @@ model = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets,
     initial_learning_rate=initial_learning_rate,
-    damping=3e-2,
+    damping=damping,
     decay_epochs=decay_epochs,
     mini_batch=True,
-    train_dir=f'trained_models/output_count{model_count}', 
+    train_dir=f'trained_models_method1/output_count{model_count}', 
     log_dir=f'throw/log{model_count}',
-    hvp_files = f"inverse_HVP_adult/inverse_HVP_schm{scheme}_count{model_count}",
+    hvp_files = f"inverse_HVP_adult_method1/inverse_HVP_schm{scheme}_count{model_count}",
     model_name=name,
     scheme = f"{scheme}")
 
 
-num_steps = 10000
 if train:
     model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=True, verbose=False)
 
-ranked_influential_training_points = f"ranking_points_ordered/{name}.npy"
-if not os.path.exists("ranking_points_ordered"):
-    os.mkdir("ranking_points_ordered")
+ranked_influential_training_points = f"ranking_points_ordered_method1/{name}.npy"
+if not os.path.exists("ranking_points_ordered_method1"):
+    os.mkdir("ranking_points_ordered_method1")
 # if not train and ranking of influential training points is stored in numpy file, then True
 load_from_numpy = False if train else (True if os.path.exists(ranked_influential_training_points) else False)       
-assert(load_from_numpy)
+# assert(load_from_numpy)
 class0_data, class1_data = entire_test_suite(mini=False)     # False means loads entire data
 if not load_from_numpy:
     if not train:
@@ -103,8 +101,6 @@ if not load_from_numpy:
         model.load_checkpoint(iter_to_load=iter_to_load, do_checks=False)
 
     model.find_discm_examples(class0_data, class1_data, print_file=True, scheme=scheme)
-
-    # test_idx = 0
 
     predicted_loss_diffs = model.get_influence_on_test_loss(
                 [i for i in range(model.discm_data_set.num_examples)], 
@@ -123,11 +119,13 @@ if not load_from_numpy:
 
 else:
    print("Loading from numpy")
-#    initial_num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
-#    print(initial_num, "See")
-#    size = class0_data.shape[0]/100
-#    with open("results_adult_noremoval.csv".format(scheme), "a") as f:
-#         f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{initial_num},{initial_num/size}\n")
+   if modify_test:
+        initial_num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
+        train_acc, test_acc = model.print_model_eval()
+        size = class0_data.shape[0]/100
+        with open("results_adult_noremoval.csv".format(scheme), "a") as f:
+                f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{initial_num},{initial_num/size}\n")
+        exit(0)
    sorted_training_points = list(np.load(ranked_influential_training_points))
 
 if train:
@@ -137,22 +135,24 @@ if train:
 # for percentage in range(5, 4, 0.5):
 # for percentage in np.arange(0, 5.0, 0.2):
 removal = int(sys.argv[2])
-# import ipdb; ipdb.set_trace()
-# for p in removal:
-train_size = 45222
+percentage = removal/5.0
+
+train_size = 36000
+dataset = "adult"
 size = class0_data.shape[0]/100
-for percentage in np.linspace(removal-1, removal-0.2, 5):
-    tf.reset_default_graph()
-    p = int(train_size * percentage / 100)
-    remaining_indexes = np.array(sorted_training_points[p:])
-    data_sets_partial = load_adult_income_partial(perm=perm, index=remaining_indexes)
-    try:
-        assert(len(remaining_indexes) == train_size - p)
-        assert(data_sets_partial.train.num_examples == train_size - p)
-    except:
-        print(p, percentage, removal, data_sets_partial.train.num_examples, "hello")
-        assert False
-    model_partial_data = Fully_Connected(
+
+# for  in np.linspace(removal-1, removal-0.2, 5):
+tf.reset_default_graph()
+p = int(train_size * percentage / 100)
+remaining_indexes = np.array(sorted_training_points[p:])
+data_sets_partial = load_adult_income_partial(perm=perm, index=remaining_indexes)
+try:
+    assert(len(remaining_indexes) == train_size - p)
+    assert(data_sets_partial.train.num_examples == train_size - p)
+except:
+    print(p, percentage, removal, data_sets_partial.train.num_examples, "hello")
+    assert False
+model_partial_data = Fully_Connected(
         input_dim=input_dim, 
         hidden1_units=hidden1_units, 
         hidden2_units=hidden2_units,
@@ -162,7 +162,7 @@ for percentage in np.linspace(removal-1, removal-0.2, 5):
         batch_size=batch_size,
         data_sets=data_sets_partial,
         initial_learning_rate=initial_learning_rate,
-        damping=1e-2,
+        damping=damping,
         decay_epochs=decay_epochs,
         mini_batch=True,
         train_dir='output_partial', 
@@ -170,15 +170,13 @@ for percentage in np.linspace(removal-1, removal-0.2, 5):
         hvp_files = "inverse_HVP_scheme1_",
         model_name='adult_income_partial',
         scheme = "scheme8_par")
-    print("Training")
-    print("Percentage: ", percentage, " Points removed: ", p) 
-    model_partial_data.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
-    # train_acc, test_acc = model_partial_data.print_model_eval()
-    # print("Percentage: ", percentage, " Points removed: ", p)
-    num = model_partial_data.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
-    with open("results_adult_full.csv", "a") as f:
-        f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{percentage},{p},{num},{num/size}\n")     # the last ones gives percentage of discrimination
-    
-    del model_partial_data          # to remove any chance of reusing variables and reduce memory
+print("Training")
+print("Percentage: ", percentage, " Points removed: ", p) 
+model_partial_data.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
+# train_acc, test_acc = model_partial_data.print_model_eval()
+# print("Percentage: ", percentage, " Points removed: ", p)
+num = model_partial_data.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
+with open(f"results_{dataset}_debiasedtrain_80percentof_total.csv", "a") as f:
+    f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{percentage},{p},{num},{num/size}\n")     # the last ones gives percentage of discrimination
 
-
+del model_partial_data          # to remove any chance of reusing variables and reduce memory
