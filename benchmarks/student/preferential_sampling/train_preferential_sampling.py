b4e6b73181f13bc9ff3298ab6f2e124ab3459899
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-12 18:08:20 -0700

    Added preferential sampling results for student

--- benchmarks/student/preferential_sampling/train_preferential_sampling.py
+++ benchmarks/student/preferential_sampling/train_preferential_sampling.py
@@ -15,14 +15,15 @@ from find_discm_points import entire_test_suite
 
 
 input_dim = 32
-weight_decay = 0.002
+weight_decay = 0.01
 # batch_size = 3000
 
-initial_learning_rate = 1e-3 
+initial_learning_rate = 1e-4 
 decay_epochs = [20000, 30000]
-num_steps = 10000
+num_steps = 15000
 num_classes = 2
 keep_probs = [1.0, 1.0]
+
 scheme = 8
 assert(scheme == 8)     # now always
 
@@ -33,15 +34,22 @@ def variation(setting_now):
     for perm in range(20):
         for h1units in [16, 24, 32]:
             for h2units in [8, 12]:
-                for batch in [50, 100]:
+                for batch in [64, 128]:      # different batch sizes for this dataset
                     if model_count < setting_now:
                         model_count += 1
                         continue
                     # print(setting_now, "done", perm, h1units, h2units, batch)
                     return perm, h1units, h2units, batch, model_count
 
+
 perm, h1units, h2units, batch, model_count = variation(setting_now)
 assert(model_count == setting_now)
+hidden1_units = h1units
+hidden2_units = h2units
+hidden3_units = 0
+batch_size = batch
+damping = 8e-2
+
 data_sets_init, x_both = before_preferential_sampling(perm = perm)
 
 male_good_credit_indices = x_both[(1, 1)]
@@ -49,10 +57,6 @@ male_bad_credit_indices = x_both[(1, 0)]
 female_good_credit_indices = x_both[(0, 1)]
 female_bad_credit_indices = x_both[(0, 0)]
 
-hidden1_units = h1units
-hidden2_units = h2units
-hidden3_units = 0
-batch_size = batch
 print("Start: ", model_count, " Setting: ", perm, hidden1_units, hidden2_units, batch_size)
 
 model = Fully_Connected(
@@ -65,7 +69,7 @@ model = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets_init,
     initial_learning_rate=initial_learning_rate,
-    damping=1e-2,
+    damping=damping,
     decay_epochs=decay_epochs,
     mini_batch=True,
     train_dir=f'throw/output_dont_save{model_count}', 
@@ -93,7 +97,6 @@ fav_pos_candidates = female_good_credit_indices[np.argsort(losses[female_good_cr
 
 data_sets_final = resampled_dataset(perm, dep_neg_candidates, dep_pos_candidates, fav_neg_candidates, fav_pos_candidates)
 
-# import ipdb; ipdb.set_trace()
 model_ = Fully_Connected(
     input_dim=input_dim, 
     hidden1_units=hidden1_units, 
@@ -104,7 +107,7 @@ model_ = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets_final,
     initial_learning_rate=initial_learning_rate,
-    damping=1e-2,
+    damping=damping,
     decay_epochs=decay_epochs,
     mini_batch=True,
     train_dir=f'throw/output_dont_save{model_count}', 
@@ -115,9 +118,6 @@ model_ = Fully_Connected(
     )
 
 model_.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, iter_to_switch_to_sgd=20000, save_checkpoints=False, verbose=False)
-
-# iter_to_load = num_steps - 1
-# train_acc, test_acc = model.load_checkpoint(iter_to_load=iter_to_load)
 class0_data, class1_data = entire_test_suite(mini=False, disparateremoved=False)     # False means loads entire data
 num_dicsm = model_.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
 train_acc, test_acc = model_.print_model_eval()
@@ -125,4 +125,4 @@ train_acc, test_acc = model_.print_model_eval()
 print("Discrimination:", num_dicsm)
 size = class0_data.shape[0]/100
 with open("results_resampling_student.csv", "a") as f:
-    f.write(f'{h1units},{h2units},{batch},{perm},{train_acc*100},{test_acc*100},{num_dicsm},{num_dicsm/size}\n')
+    print(f'{h1units},{h2units},{batch},{perm},{train_acc},{test_acc},{num_dicsm},{num_dicsm/size}', file=f)
