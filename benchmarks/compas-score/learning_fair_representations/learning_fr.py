11ecb2a42b89abe3383ec62158304cb1cf119fe2
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-14 03:08:20 -0700

    real accuracy LFR

--- benchmarks/compas-score/learning_fair_representations/learning_fr.py
+++ benchmarks/compas-score/learning_fair_representations/learning_fr.py
@@ -13,6 +13,8 @@ from influence.fully_connected import Fully_Connected
 from aif360.datasets import MyCompasScoreDataset
 from aif360.algorithms.preprocessing.lfr import LFR
 
+real_accuracy = True
+
 input_dim = 10
 weight_decay = 0.002
 # batch_size = 3000
@@ -68,7 +70,6 @@ new_df = dataset_transf_train.convert_to_dataframe()[0]
 train_labels = new_df['compas_score'].to_numpy()
 train_features = new_df.drop(columns=['compas_score']).to_numpy()
 
-
 write = False
 if write:
     with open("see.csv", "w") as f:
@@ -77,7 +78,7 @@ if write:
 from load_compas_score_as_labels import load_fair_representations
 from find_discm_points import entire_test_suite
 
-data_sets = load_fair_representations(perm, train_features, train_labels)
+data_sets = load_fair_representations(perm, train_features, train_labels, real_accuracy = real_accuracy)
 
 hidden1_units = h1units
 hidden2_units = h2units
@@ -114,5 +115,11 @@ num_dicsm = model.find_discm_examples(class0_data, class1_data, print_file=False
 print("Discrimination:", num_dicsm)
 size = class0_data.shape[0]/100
 dataset = "compas-score"
-with open(f"results_lfr_{dataset}.csv", "a") as f:
-    print(f'{h1units},{h2units},{batch},{perm},{train_acc},{test_acc},{num_dicsm},{num_dicsm/size}', file=f)
+if not real_accuracy:
+    with open(f"results_lfr_{dataset}.csv", "a") as f:
+        print(f'{h1units},{h2units},{batch},{perm},{train_acc},{test_acc},{num_dicsm},{num_dicsm/size}', file=f)
+
+if real_accuracy:
+    with open(f"results_lfr_{dataset}_real_accuracy.csv", "a") as f:
+        print(f'{model_count},{h1units},{h2units},{batch},{perm},{test_acc}', file=f)
+
