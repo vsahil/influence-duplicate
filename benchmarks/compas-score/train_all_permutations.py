99c88f8d54eb2c6e70ee212e3f00fd67d0eeeba0
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-18 19:08:03 -0700

    Added statistical parity for the full dataset, for compas-score

--- benchmarks/compas-score/train_all_permutations.py
+++ benchmarks/compas-score/train_all_permutations.py
@@ -19,7 +19,8 @@ from load_compas_score_as_labels import load_compas_two_year, load_compas_two_ye
 from find_discm_points import entire_test_suite
 
 train = False
-modify_test = True
+full_test = True
+debiased_test = True
 
 if not train:
     x = len(os.listdir('ranking_points_ordered_method1'))
@@ -59,7 +60,7 @@ hidden1_units = h1units
 hidden2_units = h2units
 hidden3_units = 0
 batch_size = batch
-data_sets = load_compas_two_year(perm, modify_test=modify_test)
+data_sets = load_compas_two_year(perm, debiased_test=debiased_test)
 
 
 print("Start: ", model_count, " Setting: ", perm, hidden1_units, hidden2_units, batch_size)
@@ -124,14 +125,47 @@ if not load_from_numpy:
 
 else:
     print("Loading from numpy")
-    if modify_test:
+    if full_test:
+        dataset = "compas-score"
         iter_to_load = num_steps - 1
         model.load_checkpoint(iter_to_load=iter_to_load, do_checks=False)
         initial_num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
-        train_acc, test_acc, _ = model.print_model_eval()
+        # train_acc, test_acc, _ = model.print_model_eval()
+        if True:        # added this just so that the code folds
+            sensitive_attr = 2
+            assert len(np.unique(data_sets.test.x[:, sensitive_attr])) == 2
+            class0_index = (data_sets.test.x[:, sensitive_attr] == 0).astype(int).nonzero()[0]
+            class1_index = (data_sets.test.x[:, sensitive_attr] == 1).astype(int).nonzero()[0]
+            train_acc, test_acc, test_predictions = model.print_model_eval()
+            test_predictions = np.argmax(test_predictions, axis=1)
+            class0_pred = test_predictions[class0_index]
+            class1_pred = test_predictions[class1_index]
+            class0_truth = data_sets.test.labels[class0_index]
+            class1_truth = data_sets.test.labels[class1_index]
+            assert(len(class0_pred) + len(class1_pred) == len(test_predictions))
+            assert(len(class0_truth) + len(class1_truth) == len(data_sets.test.labels))
+            import sklearn
+            class0_cm = sklearn.metrics.confusion_matrix(class0_truth, class0_pred)
+            class1_cm = sklearn.metrics.confusion_matrix(class1_truth, class1_pred)
+            tn, fp, fn, tp = class0_cm.ravel()
+            class0_fpr = fp / (fp + tn)
+            class0_fnr = fn / (fn + tp)
+            class0_pos = (tp + fp) / len(class0_index)        # proportion that got positive outcome
+            del tn, fp, fn, tp
+            tn, fp, fn, tp = class1_cm.ravel()
+            class1_fpr = fp / (fp + tn)
+            class1_fnr = fn / (fn + tp)
+            class1_pos = (tp + fp) / len(class1_index)        # proportion that got positive outcome
+
         size = class0_data.shape[0]/100
-        with open("results_compas-score_noremoval.csv".format(scheme), "a") as f:
-                f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{initial_num},{initial_num/size}\n")
+        if debiased_test:
+            with open(f"results_{dataset}_noremoval.csv", "a") as f:
+                print(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{class0_fpr},{class0_fnr},{class0_pos},{class1_fpr},{class1_fnr},{class1_pos},{initial_num},{initial_num/size}", file=f)
+        else:
+            with open(f"results_{dataset}_noremoval_fulltest.csv", "a") as f:
+                print(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{class0_fpr},{class0_fnr},{class0_pos},{class1_fpr},{class1_fnr},{class1_pos},{initial_num},{initial_num/size}", file=f)
+        # with open("results_compas-score_noremoval.csv".format(scheme), "a") as f:
+        #         f.write(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{initial_num},{initial_num/size}\n")
         exit(0)
     sorted_training_points = list(np.load(ranked_influential_training_points))
 
