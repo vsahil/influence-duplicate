01d18470c74b1b7e33231b3e9121ccc4ca132aaa
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-19 21:01:02 -0700

    results of our approach for adult race

--- benchmarks/adult-race/check_parallel.py
+++ benchmarks/adult-race/check_parallel.py
@@ -7,7 +7,7 @@ def experiment_command(setting, removal_percent):
     os.system(f"python -W ignore train_all_permutations.py {setting} {removal_percent}")
 
 # pool = multiprocessing.Pool(120)
-# l = [i for i in range(26*5, 51*5)]      # upto 25% removal in steps of 0.2%, from 27 to 40.6 minimum discrimination 
+# l = [i for i in range(1*5, 51*5)]      # upto 25% removal in steps of 0.2%, from 27 to 40.6 minimum discrimination 
 # # mr = pool.map_async(run_command, l)
 # settings = [i for i in range(240)]       # for first 120 settings in total
 # mr = pool.starmap_async(experiment_command, product(settings, l))
@@ -18,9 +18,9 @@ def experiment_command(setting, removal_percent):
 
 
 def run_command(setting):
-    # os.system(f"python train_all_permutations.py {setting}")
+    os.system(f"python train_all_permutations.py {setting}")
     # os.system(f"python methodology1.py {setting}")
-    os.system(f"python train_all_permutations_nosensitive.py {setting}")
+    # os.system(f"python train_all_permutations_nosensitive.py {setting}")
 
 
 pool = multiprocessing.Pool(120)
