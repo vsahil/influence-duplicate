899b2f52299dd9b8dd9c3aa1c5ce6bd0c0ccd4e0
Sahil Verma <vsahil@.cs.washington.edu>
2020-05-20 22:57:13 -0700

    results for our approach, training for higher number of epochs didn't bring any change, so we abandoned it

--- benchmarks/default/methodology1.py
+++ benchmarks/default/methodology1.py
@@ -20,10 +20,14 @@ from find_discm_points import entire_test_suite
 
 input_dim = 23
 weight_decay = 0.001
-
+# batch_size = 3000
 initial_learning_rate = 1e-4 
 decay_epochs = [40000, 50000]
 num_steps = 10000
+
+# initial_learning_rate = 1e-2 
+# decay_epochs = [10000, 15000]
+# num_steps = 20000
 num_classes = 2
 keep_probs = [1.0, 1.0]
 
@@ -51,17 +55,21 @@ hidden1_units = h1units
 hidden2_units = h2units
 hidden3_units = 0
 batch_size = batch
+damping = 3e-2
+debiased_test = False
 
 name = f"default_{model_count}"
 
 import pandas as pd
 dataset = "default"
-df = pd.read_csv(f"results_{dataset}_debiasedtrain_80percentof_total.csv")
-removal_df = df.sort_values(by=['Discm_percent', 'Points-removed']).groupby("Model-count", as_index=False).first()
+# df = pd.read_csv(f"results_{dataset}_debiasedtrain_80percentof_total.csv")
+# removal_df = df.sort_values(by=['Discm_percent', 'Points-removed']).groupby("Model-count", as_index=False).first()
+removal_df = pd.read_csv(f"removal_df_{dataset}.csv")
 assert len(removal_df) == 240
 train_pts_removed = removal_df.loc[removal_df['Model-count'] == model_count, 'Points-removed'].values
 assert len(train_pts_removed) == 1
-data_sets = load_default_partial_method1(perm=perm, model_count=model_count, train_pts_removed=train_pts_removed[0], name=name)
+print(train_pts_removed[0], "see removed pts")
+data_sets = load_default_partial_method1(perm=perm, model_count=model_count, train_pts_removed=train_pts_removed[0], name=name, debiased_test=debiased_test)
 print("Training points removed:", train_pts_removed[0])
 training_size = 24000
 percentage = train_pts_removed[0]*100.0/training_size
@@ -77,14 +85,14 @@ model = Fully_Connected(
     batch_size=batch_size,
     data_sets=data_sets,
     initial_learning_rate=initial_learning_rate,
-    damping=3e-2,
+    damping=damping,
     decay_epochs=decay_epochs,
     mini_batch=True,
     train_dir=f'trained_models_try', 
     log_dir=f'throw',
-    hvp_files = f'inverse_HVP_student_try',
-    model_name=name,
-    scheme = f"{scheme}")
+    hvp_files = f'inverse_HVP_default_try',
+    model_name='default_partial',
+    scheme = "scheme8_par")
 
 
 model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000, 
@@ -93,12 +101,15 @@ model.train(num_steps=num_steps, iter_to_switch_to_batch=10000000,
 class0_data, class1_data = entire_test_suite(mini=False)     # False means loads entire data
 size = class0_data.shape[0]/100.0
 num = model.find_discm_examples(class0_data, class1_data, print_file=False, scheme=scheme)
+assert (num == 0)
 
 sensitive_attr = 1
+train_acc, test_acc, test_predictions = model.print_model_eval()
+import sklearn, math
 assert len(np.unique(data_sets.test.x[:, sensitive_attr])) == 2
+# import ipdb; ipdb.set_trace()
 class0_index = (data_sets.test.x[:, sensitive_attr] == 0).astype(int).nonzero()[0]
 class1_index = (data_sets.test.x[:, sensitive_attr] == 1).astype(int).nonzero()[0]
-train_acc, test_acc, test_predictions = model.print_model_eval()
 test_predictions = np.argmax(test_predictions, axis=1)
 class0_pred = test_predictions[class0_index]
 class1_pred = test_predictions[class1_index]
@@ -106,17 +117,22 @@ class0_truth = data_sets.test.labels[class0_index]
 class1_truth = data_sets.test.labels[class1_index]
 assert(len(class0_pred) + len(class1_pred) == len(test_predictions))
 assert(len(class0_truth) + len(class1_truth) == len(data_sets.test.labels))
-import sklearn
-class0_cm = sklearn.metrics.confusion_matrix(class0_truth, class0_pred)
-class1_cm = sklearn.metrics.confusion_matrix(class1_truth, class1_pred)
+
+class0_cm = sklearn.metrics.confusion_matrix(class0_truth, class0_pred, labels=[0,1])
+class1_cm = sklearn.metrics.confusion_matrix(class1_truth, class1_pred, labels=[0,1])
 tn, fp, fn, tp = class0_cm.ravel()
 class0_fpr = fp / (fp + tn)
 class0_fnr = fn / (fn + tp)
+class0_pos = (tp + fp) / len(class0_index)        # proportion that got positive outcome
 del tn, fp, fn, tp
 tn, fp, fn, tp = class1_cm.ravel()
 class1_fpr = fp / (fp + tn)
 class1_fnr = fn / (fn + tp)
+class1_pos = (tp + fp) / len(class1_index)        # proportion that got positive outcome
 
-with open(f"results_{dataset}_method1.csv".format(scheme), "a") as f:
-    print(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{class0_fpr},{class0_fnr},{class1_fpr},{class1_fnr},{percentage},{train_pts_removed[0]},{num},{num/size}", file=f)
-
+if debiased_test:
+    with open(f"results_{dataset}_method1.csv".format(scheme), "a") as f:
+        print(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{class0_fpr},{class0_fnr},{class0_pos},{class1_fpr},{class1_fnr},{class1_pos},{percentage},{train_pts_removed[0]},{num},{num/size}", file=f)
+else:
+    with open(f"results_{dataset}_method1_fulltest.csv".format(scheme), "a") as f:
+        print(f"{model_count},{perm},{h1units},{h2units},{batch},{train_acc},{test_acc},{class0_fpr},{class0_fnr},{class0_pos},{class1_fpr},{class1_fnr},{class1_pos},{percentage},{train_pts_removed[0]},{num},{num/size}", file=f)
